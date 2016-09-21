2db56963f6c4b43c617510ee03f25cda5ba4afaf
Amarendra9 <amar442d@gmail.com>
2016-09-21 22:09:19 +0530

    Run.java is added

---
+++ Run.java
@@ -0,0 +1,11 @@
+package CorePrograms;
+
+   class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("welcome");
+ System.out.println("run on server");
+	}
+
+}
